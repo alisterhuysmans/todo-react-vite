8f75546584d6042de17547c7bb557dcfff8432f7
Alister Huysmans <alister.huysmans@gmail.com>
2023-03-09 16:59:47 +0100

    implement save todo to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,48 @@ const App = () => {
     const [todos, setTodos] = useState([]);
     const [status, setStatus] = useState("all");
     const [filteredTodos, setFilteredTodos] = useState([]);
+
+    // run once when the app starts
+    useEffect(() => {
+        getLocalTodos();
+    }, []);
+
     // use effect
     useEffect(() => {
-        const filterHandler = () => {
-            switch (status) {
-                case "completed":
-                    setFilteredTodos(
-                        todos.filter((todo) => todo.completed === true)
-                    );
-                    break;
-                case "uncompleted":
-                    setFilteredTodos(
-                        todos.filter((todo) => todo.completed === false)
-                    );
-                    break;
-                default:
-                    setFilteredTodos(todos);
-                    break;
-            }
-        };
         filterHandler();
+        saveLocalTodos();
     }, [todos, status]);
+
+    const filterHandler = () => {
+        switch (status) {
+            case "completed":
+                setFilteredTodos(
+                    todos.filter((todo) => todo.completed === true)
+                );
+                break;
+            case "uncompleted":
+                setFilteredTodos(
+                    todos.filter((todo) => todo.completed === false)
+                );
+                break;
+            default:
+                setFilteredTodos(todos);
+                break;
+        }
+    };
+
+    // save to local
+    const saveLocalTodos = () => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    };
+    const getLocalTodos = () => {
+        if (localStorage.getItem("todos") === null) {
+            localStorage.setItem("todos", JSON.stringify([]));
+        } else {
+            let todoLocal = JSON.parse(localStorage.getItem("todos"));
+            setTodos(todoLocal);
+        }
+    };
 
     return (
         <div className="App">
